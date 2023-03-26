e1697bf6b12b8056c0e0ac5515010bc516bda55a
gabriele-giannotto <gabriele.giannotto@mail.polimi.it>
2023-03-26 01:30:51 +0100

    Add hours when phone must be set to Mute mode

--- app/src/main/java/com/example/mutemode/RunnableSetMuteMode.java
+++ app/src/main/java/com/example/mutemode/RunnableSetMuteMode.java
@@ -5,31 +5,54 @@ import android.media.AudioManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * extends AppCompatActivity is necessary??????
  * This class is used to set modeOFF/ON
  */
 public class RunnableSetMuteMode  implements Runnable {
     AudioManager aManager;
-
+    boolean setMuteMode;
     public RunnableSetMuteMode(){
         aManager = MainActivity.aManager;
-
+        setMuteMode = false;
     }
     @Override
     public void run() {
 
             try {
                 while (true){
-                     if(aManager.getRingerMode()== AudioManager.RINGER_MODE_SILENT){
-                aManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-            }else{
-                aManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-            }
-
-
-                    System.out.println("OUTPUT 123");
-                    Thread.sleep(2000);
+                    System.out.println(Calendar.getInstance(Locale.ITALY).getTime().getDay());
+                    int day = Calendar.getInstance(Locale.ITALY).getTime().getDay();
+                    int hour = Calendar.getInstance(Locale.ITALY).getTime().getHours();
+                    int minute = Calendar.getInstance(Locale.ITALY).getTime().getMinutes();
+                    if((day == 1 && hour==13 && minute>=30 )||(day == 1 && hour>13 && hour <15 ) ||(day==1 && hour==15 && minute==0)){
+                        setMuteMode = true;
+                    } else if ((day == 2 && hour==9 && minute>=30 )||(day == 2 && hour>9 && hour <11 ) ||(day==2 && hour==11 && minute==0)) {
+                        setMuteMode = true;
+                    }else if ((day == 2 && hour==11 && minute>=30 )||(day == 2 && hour>11 && hour <13 ) ||(day==2 && hour==13 && minute==0)) {
+                        setMuteMode = true;
+                    }else if ((day == 3 && hour==13 && minute>=30 )||(day == 3 && hour>13 && hour < 15) ||(day==3 && hour==15 && minute==0)) {
+                        setMuteMode = true;
+                    }
+                    else if ((day == 3 && hour==15 && minute>=30 )||(day == 3 && hour>15 && hour < 18) ||(day==3 && hour==18 && minute==0)) {
+                        setMuteMode = true;
+                    }else if ((day == 4 && hour==16 && minute>=30 )||(day == 4 && hour>16 && hour < 18) ||(day==4 && hour==18 && minute==0)) {
+                        setMuteMode = true;
+                    }else{
+                        setMuteMode = false;
+                    }
+                    // This check if phone must be set to mute/unmute mode
+                    if (setMuteMode == false &&(aManager.getRingerMode()== AudioManager.RINGER_MODE_SILENT )){
+                        aManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                    }else if ((setMuteMode == true) &&(aManager.getRingerMode()== AudioManager.RINGER_MODE_NORMAL )){
+                        aManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                    }
+                   // System.out.println("OUTPUT 123");
+                    Thread.sleep(1000);
                 }
 
             } catch (InterruptedException e) {
